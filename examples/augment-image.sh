4d21d0845253dcc9e7c7ca7f3191d1eba39b1510
Gareth Rushgrove <gareth@morethanseven.net>
2017-05-09 14:21:26 +0100

    (MAINT) Added examples centered around image manipulation

---
+++ examples/augment-image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# This script augments a specified Docker image with a static inventory of information
+# It does this by booting a container based on the image, generating the inventory and
+# then saving a new version of the image with an additional layer containing the inventory
+# at /inventory.json
+# This allows for accessing the inventory in any instance of that image, for instance:
+# docker run --rm <image> cat /inventory.json
+
+IMAGE=${1?"Usage: $0 IMAGE[:TAG]"}
+
+CONTAINER_ID=$(docker run --rm -di --entrypoint "" "$IMAGE" /bin/sh -c "while true; do echo hello world; sleep 1; done")
+docker run --rm  -v /var/run/docker.sock:/var/run/docker.sock local/tc container "$CONTAINER_ID" > output.json
+docker cp ./output.json "$CONTAINER_ID":/lumogon.json
+docker commit "$CONTAINER_ID" "$IMAGE"
+docker kill "$CONTAINER_ID"
+rm ./output.json
