b4e18947d3c985ec8fb787e42364d20770d01c57
Gareth Rushgrove <gareth@morethanseven.net>
2017-05-16 14:37:27 +0100

    (MAINT) Add the hostname on which the report was generated
    This came up talking to a user today. We'll need to consider this and
    a few of the other top level parameters when we look at Swarm and
    multi-host reports but feels useful somewhere. This might want a
    clearer name?

--- collector/report.go
+++ collector/report.go
@@ -1,6 +1,7 @@
 package collector
 
 import (
+	"os"
 	"time"
 
 	"github.com/puppetlabs/lumogon/types"
@@ -9,9 +10,11 @@ import (
 
 // NewReport gathers data from all capabilities
 func NewReport(reportID string, clientVersion version.ClientVersion) *types.Report {
+	hostname, _ := os.Hostname()
 	report := types.Report{
 		Schema:        "http://puppet.com/lumogon/core/draft-01/schema#1",
 		Generated:     time.Now().String(),
+		Host:          hostname,
 		Owner:         "default",
 		Group:         []string{"default"},
 		ClientVersion: clientVersion,
