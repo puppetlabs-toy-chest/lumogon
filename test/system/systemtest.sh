4e8b81e9c667496fd1074691821f0b40fcd06092
John McCabe <john@johnmccabe.net>
2017-06-21 18:50:13 +0100

    (MAINT) cache test nginx image
    This commit caches the test nginx image to disk and loads it locally
    rather than having to pull the image from within each DIND container.
    
    This speeds up the execution as you're only pulling in from the network
    once.
    
    It also sidesteps the fact that Docker 1.5.0 doesn't work with the Hub
    so would fail to pull the image.

--- test/system/systemtest.sh
+++ test/system/systemtest.sh
@@ -11,8 +11,12 @@ MISSING_DIND_IMAGE="johnmccabe/dind"
 TITLE="\033[104m" # white on light blue
 NC="\033[0m" # no colour
 
-echo "Saving local ${IMAGE_NAME} image to disk..."
+echo "Saving local image ${IMAGE_NAME} to disk..."
 docker save ${IMAGE_NAME} > ${PWD}/lumogon.tar
+
+echo "Saving test image ${TEST_IMAGE} to disk..."
+docker pull ${TEST_IMAGE}
+docker save ${TEST_IMAGE} > ${PWD}/testimage.tar
 
 echo "Running ${IMAGE_NAME} against official Docker DIND images"
 for v in "${DIND_VERSIONS[@]}"
@@ -20,6 +24,7 @@ do
     echo -e "  - ${TITLE}Docker version ${v}${NC}"
     docker run --rm -d --privileged --name dind-${v} docker:${v}-dind
     docker run --rm -e DOCKER_HOST=tcp://docker:2375 -v ${PWD}/lumogon.tar:/lumogon.tar --link dind-${v}:docker ${OFFICIAL_DIND_IMAGE}:${v} docker load -i /lumogon.tar
+    docker run --rm -e DOCKER_HOST=tcp://docker:2375 -v ${PWD}/testimage.tar:/testimage.tar --link dind-${v}:docker ${OFFICIAL_DIND_IMAGE}:${v} docker load -i /testimage.tar
     docker run --rm -e DOCKER_HOST=tcp://docker:2375 --link dind-${v}:docker ${OFFICIAL_DIND_IMAGE}:${v} docker run -d --name nginx ${TEST_IMAGE}
     docker run --rm -e DOCKER_HOST=tcp://docker:2375 --link dind-${v}:docker ${OFFICIAL_DIND_IMAGE}:${v} docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ${IMAGE_NAME} scan
     docker kill dind-${v}
@@ -31,6 +36,7 @@ do
     echo -e "  - ${TITLE}Docker version ${v}${NC}"
     docker run --rm -d --privileged --name dind-${v} ${MISSING_DIND_IMAGE}:${v}
     docker run --rm -e DOCKER_HOST=tcp://docker:2375 -v ${PWD}/lumogon.tar:/lumogon.tar --link dind-${v}:docker ${MISSING_DIND_IMAGE}:${v} docker load -i /lumogon.tar
+    docker run --rm -e DOCKER_HOST=tcp://docker:2375 -v ${PWD}/testimage.tar:/testimage.tar --link dind-${v}:docker ${OFFICIAL_DIND_IMAGE}:${v} docker load -i /testimage.tar
     docker run --rm -e DOCKER_HOST=tcp://docker:2375 --link dind-${v}:docker ${MISSING_DIND_IMAGE}:${v} docker run -d --name nginx ${TEST_IMAGE}
     docker run --rm -e DOCKER_HOST=tcp://docker:2375 --link dind-${v}:docker ${MISSING_DIND_IMAGE}:${v} docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ${IMAGE_NAME} scan
     docker kill dind-${v}
