692040ff678351d9ba4b8d9dcd0438aae1a08698
John McCabe <john@johnmccabe.net>
2017-06-22 16:14:37 +0100

    (MAINT) run in function and attempt to validate
    This is a WIP commit, it doesn't work fully as intended, but making
    visible early

--- test/systemtest/systemtest.sh
+++ test/systemtest/systemtest.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+###############################################################################
+#   Constants
+###############################################################################
 DIND_VERSIONS=( 17.06.0 17.05.0 17.04.0 17.03.2 1.13.1 1.12.6 1.11.2 )
 MISSIND_DIND_VERSIONS=( 1.10.3 1.9.1 1.8.3 1.7.1 1.6.2 1.5.0 )
 
@@ -9,37 +12,118 @@ OFFICIAL_DIND_IMAGE="docker"
 MISSING_DIND_IMAGE="johnmccabe/dind"
 
 TITLE="\033[104m" # white on light blue
+PASS="\033[42m"
+FAIL="\033[41m"
 NC="\033[0m" # no colour
 
-echo "Saving local image ${IMAGE_NAME} to disk..."
-docker save ${IMAGE_NAME} > ${PWD}/lumogon.tar
+###############################################################################
+#   Variables
+###############################################################################
+PASSED=()
+FAILED=()
+LUMODEBUG="${LUMODEBUG:-0}"
+REDIRECT=""
 
-echo "Saving test image ${TEST_IMAGE} to disk..."
-docker pull ${TEST_IMAGE}
-docker save ${TEST_IMAGE} > ${PWD}/testimage.tar
+
+###############################################################################
+#   Functions
+###############################################################################
+
+function runLumogon()
+{
+    local dind_image=$1
+    local dind_version=$2
+    local dind_version_suffix=$3
+    local current_dind_image="${dind_image}:${dind_version}${dind_version_suffix}"
+    local scan_output=/tmp/dind-${dind_version}.lumoscan
+
+    echo "============================================"
+    echo -e "${TITLE}Docker version ${dind_version}${NC}"
+    docker run --rm -d --privileged \
+        --name dind-${dind_version} \
+        ${dind_image}:${dind_version}${dind_version_suffix}
+
+    docker run --rm \
+        -e DOCKER_HOST=tcp://docker:2375 \
+        -v ${PWD}/lumogon.tar:/lumogon.tar \
+        --link dind-${dind_version}:docker \
+        ${dind_image}:${dind_version}\
+        docker load -i /lumogon.tar 
+
+    docker run --rm -e \
+        DOCKER_HOST=tcp://docker:2375 \
+        -v ${PWD}/testimage.tar:/testimage.tar \
+        --link dind-${dind_version}:docker \
+        ${dind_image}:${dind_version} \
+        docker load -i /testimage.tar
+
+    docker run --rm \
+        -e DOCKER_HOST=tcp://docker:2375 \
+        --link dind-${dind_version}:docker \
+        ${dind_image}:${dind_version}\
+        docker run -d --name nginx ${TEST_IMAGE}
+
+    rm -rf ${scan_output}
+
+    docker run --rm \
+        -e DOCKER_HOST=tcp://docker:2375 \
+        --link dind-${dind_version}:docker \
+        ${dind_image}:${dind_version} \
+        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ${IMAGE_NAME} scan \
+    > ${scan_output}
+
+    docker kill dind-${v}
+}
+
+function saveImages()
+{
+    echo "Saving images to disk"
+    echo "  - ${IMAGE_NAME}"
+    docker save ${IMAGE_NAME} > ${PWD}/lumogon.tar
+
+    echo "  - ${TEST_IMAGE}"
+    docker pull ${TEST_IMAGE}
+    docker save ${TEST_IMAGE} > ${PWD}/testimage.tar
+
+}
+
+function checkNumContainers()
+{
+    local dind_version=$1
+    local scan_output=/tmp/dind-${dind_version}.lumoscan
+
+    if [ ! -f ${scan_output} ]; then
+        echo -e "  - ${FAIL}no scan file found${NC}"
+        return 1
+    fi
+
+    local numContainers=`jq '.containers | length' ${scan_output}`
+    if [ "$numContainers" != "1" ]; then
+        echo -e "  - ${FAIL}unexpected number of containers returned [${numContainers}]${NC}"
+        return 1
+    fi
+
+    echo -e "  - ${PASS}expected number of containers returned 1${NC}"
+}
+
+
+###############################################################################
+#   Main
+###############################################################################
+
+saveImages
 
 echo "Running ${IMAGE_NAME} against official Docker DIND images"
 for v in "${DIND_VERSIONS[@]}"
 do
-    echo -e "  - ${TITLE}Docker version ${v}${NC}"
-    docker run --rm -d --privileged --name dind-${v} docker:${v}-dind
-    docker run --rm -e DOCKER_HOST=tcp://docker:2375 -v ${PWD}/lumogon.tar:/lumogon.tar --link dind-${v}:docker ${OFFICIAL_DIND_IMAGE}:${v} docker load -i /lumogon.tar
-    docker run --rm -e DOCKER_HOST=tcp://docker:2375 -v ${PWD}/testimage.tar:/testimage.tar --link dind-${v}:docker ${OFFICIAL_DIND_IMAGE}:${v} docker load -i /testimage.tar
-    docker run --rm -e DOCKER_HOST=tcp://docker:2375 --link dind-${v}:docker ${OFFICIAL_DIND_IMAGE}:${v} docker run -d --name nginx ${TEST_IMAGE}
-    docker run --rm -e DOCKER_HOST=tcp://docker:2375 --link dind-${v}:docker ${OFFICIAL_DIND_IMAGE}:${v} docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ${IMAGE_NAME} scan
-    docker kill dind-${v}
+    runLumogon ${OFFICIAL_DIND_IMAGE} ${v} -dind
+    checkNumContainers ${v}
 done
 
 echo "Running ${IMAGE_NAME} against Missing DIND images"
 for v in "${MISSIND_DIND_VERSIONS[@]}"
 do
-    echo -e "  - ${TITLE}Docker version ${v}${NC}"
-    docker run --rm -d --privileged --name dind-${v} ${MISSING_DIND_IMAGE}:${v}
-    docker run --rm -e DOCKER_HOST=tcp://docker:2375 -v ${PWD}/lumogon.tar:/lumogon.tar --link dind-${v}:docker ${MISSING_DIND_IMAGE}:${v} docker load -i /lumogon.tar
-    docker run --rm -e DOCKER_HOST=tcp://docker:2375 -v ${PWD}/testimage.tar:/testimage.tar --link dind-${v}:docker ${OFFICIAL_DIND_IMAGE}:${v} docker load -i /testimage.tar
-    docker run --rm -e DOCKER_HOST=tcp://docker:2375 --link dind-${v}:docker ${MISSING_DIND_IMAGE}:${v} docker run -d --name nginx ${TEST_IMAGE}
-    docker run --rm -e DOCKER_HOST=tcp://docker:2375 --link dind-${v}:docker ${MISSING_DIND_IMAGE}:${v} docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ${IMAGE_NAME} scan
-    docker kill dind-${v}
+    runLumogon ${MISSING_DIND_IMAGE} ${v}
 done
 
 echo "Finished"
