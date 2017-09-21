3c9c91a8e21ea0db13a9bac58cc8c5fdaaa6afb3
John McCabe <john@johnmccabe.net>
2017-09-21 14:25:44 +0100

    (DI-489) Remove unused port fn and filter ints
    This commit removes an ports function and also stores the ints in the
    container capability as native JSON values.
    
    *NOTE* still storing bools as strings pending an update on the reporter
    UI side.

--- capabilities/container/container.go
+++ capabilities/container/container.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/docker/go-connections/nat"
 	"github.com/puppetlabs/lumogon/capabilities/payloadfilter"
 	"github.com/puppetlabs/lumogon/capabilities/registry"
 	"github.com/puppetlabs/lumogon/dockeradapter"
@@ -29,13 +28,13 @@ var containerCapability = dockeradapter.DockerAPICapability{
 		logging.Debug("[Container Info] Harvesting container information associated with %s [%s]\n", target.Name, target.ID)
 		capability.HarvestID = id
 
-		version, err := InspectContainer(client, target.ID)
+		containerInfo, err := InspectContainer(client, target.ID)
 		if err != nil {
 			capability.PayloadError(err.Error())
 			return
 		}
 
-		filtered, _ := payloadfilter.Filter(version)
+		filtered, _ := payloadfilter.Filter(containerInfo)
 
 		capability.Payload = filtered
 	},
@@ -69,42 +68,31 @@ func InspectContainer(client dockeradapter.Harvester, targetID string) (map[stri
 		"privileged":         fmt.Sprintf("%t", c.HostConfig.Privileged),
 		"publishallports":    fmt.Sprintf("%t", c.HostConfig.PublishAllPorts),
 		"readonlyrootfs":     fmt.Sprintf("%t", c.HostConfig.ReadonlyRootfs),
-		"shmsize":            fmt.Sprintf("%d", c.HostConfig.ShmSize),
+		"shmsize":            c.HostConfig.ShmSize,
 		"capadd":             strings.Join(c.HostConfig.CapAdd, ", "),
 		"capdrop":            strings.Join(c.HostConfig.CapDrop, ", "),
 		"runtime":            c.HostConfig.Runtime,
-		"cpushares":          fmt.Sprintf("%d", c.HostConfig.Resources.CPUShares),
-		"memory":             fmt.Sprintf("%d", c.HostConfig.Resources.Memory),
-		"nanocpus":           fmt.Sprintf("%d", c.HostConfig.Resources.NanoCPUs),
-		"cpuperiod":          fmt.Sprintf("%d", c.HostConfig.Resources.CPUPeriod),
-		"cpuquota":           fmt.Sprintf("%d", c.HostConfig.Resources.CPUQuota),
-		"cpurealtimeperiod":  fmt.Sprintf("%d", c.HostConfig.Resources.CPURealtimePeriod),
-		"cpurealtimeruntime": fmt.Sprintf("%d", c.HostConfig.Resources.CPURealtimeRuntime),
+		"cpushares":          c.HostConfig.Resources.CPUShares,
+		"memory":             c.HostConfig.Resources.Memory,
+		"nanocpus":           c.HostConfig.Resources.NanoCPUs,
+		"cpuperiod":          c.HostConfig.Resources.CPUPeriod,
+		"cpuquota":           c.HostConfig.Resources.CPUQuota,
+		"cpurealtimeperiod":  c.HostConfig.Resources.CPURealtimePeriod,
+		"cpurealtimeruntime": c.HostConfig.Resources.CPURealtimeRuntime,
 		"cpusetcpus":         c.HostConfig.Resources.CpusetCpus,
 		"cpusetmems":         c.HostConfig.Resources.CpusetMems,
-		"diskquota":          fmt.Sprintf("%d", c.HostConfig.Resources.DiskQuota),
-		"kernelmemory":       fmt.Sprintf("%d", c.HostConfig.Resources.KernelMemory),
-		"memoryreservation":  fmt.Sprintf("%d", c.HostConfig.Resources.MemoryReservation),
-		"memoryswap":         fmt.Sprintf("%d", c.HostConfig.Resources.MemorySwap),
-		"memoryswappiness":   fmt.Sprintf("%d", *c.HostConfig.Resources.MemorySwappiness),
+		"diskquota":          c.HostConfig.Resources.DiskQuota,
+		"kernelmemory":       c.HostConfig.Resources.KernelMemory,
+		"memoryreservation":  c.HostConfig.Resources.MemoryReservation,
+		"memoryswap":         c.HostConfig.Resources.MemorySwap,
+		"memoryswappiness":   *c.HostConfig.Resources.MemorySwappiness,
 		"oomkilldisable":     fmt.Sprintf("%t", *c.HostConfig.Resources.OomKillDisable),
-		"pidslimit":          fmt.Sprintf("%d", c.HostConfig.Resources.PidsLimit),
+		"pidslimit":          c.HostConfig.Resources.PidsLimit,
 	}
 
 	logging.Debug("[Container Info] Harvested [%+v]\n", result)
 
 	return result, nil
-}
-
-func ports(m nat.PortSet) []string {
-	keys := make([]string, len(m))
-
-	i := 0
-	for k := range m {
-		keys[i] = k.Port()
-		i++
-	}
-	return keys
 }
 
 func init() {
